5e4108fe31689b5d16159414204e400103fcef6b
Shresth Saxena <63297703+Scoder08@users.noreply.github.com>
2020-05-15 18:27:37 +0530

    Add files via upload

---
+++ webseries.py
@@ -0,0 +1,59 @@
+import urllib.request, urllib.parse, urllib.error
+import xml.etree.ElementTree as ET
+import ssl
+from bs4 import BeautifulSoup as bs
+import re
+
+ctx = ssl.create_default_context()
+ctx.check_hostname = False
+ctx.verify_mode = ssl.CERT_NONE
+web=input("Web series name : ")
+season=int(input("Season : "))
+episode=int(input("Episode : "))
+# to search 
+url="http://dl.sitemovie.ir/serial/"
+a=urllib.request.urlopen(url,context=ctx).read()
+soup=bs(a,'html.parser')
+L=soup.find_all('a')
+counts=0
+for i in L:
+	if i.text[:len(i.text)-1].lower()==web.lower():
+		url+=i.get('href')
+		a=urllib.request.urlopen(url,context=ctx).read()
+		soups=bs(a,'html.parser')
+		L1=soups.find_all("a")
+		try:
+			count=0
+			for j in L1:
+				a=re.findall('([0-9]+)/',j.text)
+				if len(a)>0:
+					if a[0]==str(season):
+						url+=j.get('href')
+						break
+			a=urllib.request.urlopen(url,context=ctx).read()
+			soupe=bs(a,'html.parser')
+			Le=soupe.find_all('a')
+			count=1
+			for k in Le:
+				b=re.findall('E([0-9]+)_',k.text)
+				if len(b)>0:
+					if int(b[0])==episode:
+						url+=k.get("href")
+						break
+			print(url)
+			urllib.request.urlretrieve(url, web+"S"+str(season)+"E"+str(episode)+".mkv")
+			print("\n			 downloading completed ..............") 
+		except:
+			if count==0:
+				print("Season not available")
+			else:
+				print("Episode not available")
+		break
+	else:
+		counts+=1
+		continue
+x=""
+if counts==len(L):
+	for i in range(len(L)):
+		x+=L[i].text[:-1]+"\n"
+	print(x)
