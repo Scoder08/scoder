f3648f316c6fc22781253995e5eb7efb23e7297e
Shresth Saxena <63297703+Scoder08@users.noreply.github.com>
2020-05-17 02:57:55 +0530

    Add files via upload

---
+++ yt-downloader.py
@@ -0,0 +1,23 @@
+from __future__ import unicode_literals
+from googlesearch import search
+import youtube_dl
+ydl_opts = {}
+b=0
+query=input("Search on youtube : ")
+for i in search(query,  tld = 'com',  
+    	                lang = 'en',  
+    	                num = 10,     
+    	                start = 0,    
+    	                stop = None,  
+    	                pause = 2.0,  
+    	               ):
+    if "youtube" in i:
+        with youtube_dl.YoutubeDL(ydl_opts) as ydl:
+            b=i
+
+        break
+print(b)
+try:
+    ydl.download([b])
+except:
+    print("I am sorry i can't download this video.")
